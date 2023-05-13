d525dc0842e3238d9ce2b90aaaebf88e2df42a34
Waseemk9 <39156999+Waseemk9@users.noreply.github.com>
2023-05-12 18:31:58 -0700

    Update my_file_2.java

--- my_file_2.java
+++ my_file_2.java
@@ -1,27 +1,17 @@
-/**
- * Basics Java code for Demo
- * Strings in Java by definition are immutable
- * String are inherently array of characters, but unlike array you can manipulate one character of string in place.
- *
- * 1.) Add a breakpoint
- * 2.) debug layout: open any object details
- * 3.) Add Conditional breakpoint
- * 4.) Add Exception Breakpoint
- *
- */
-public class ConditionalBreakPoint  {
+# Duplication Example
+public class Duplication  {
 
-    public void breakOnConditionOnly(){
-        String[] data1 = {"mz", "my", "my", "mx", "mz", "mx", "my", "mz", "mz", "my", "my", "mx", "mz", "mx", "my", "mz"};
+    public void example(){
+        String[] data1 = {"Duplicate", "Example"};
 
         for(int i=0;i<data1.length;i++){
             String s = "Super : "+data1[i];
             System.out.println(s);
-        }  
+        }
     }
     public static void main(String[] args) {
-        ConditionalBreakPoint test = new ConditionalBreakPoint();
-        test.breakOnConditionOnly();
+        Duplication test = new Duplication();
+        test.example();
     }
 
 }
