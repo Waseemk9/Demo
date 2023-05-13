73aa4baaa61e4f72b658a9012ed04022126ce50f
Waseemk9 <39156999+Waseemk9@users.noreply.github.com>
2023-05-12 18:34:22 -0700

    Update my_file.java

--- my_file.java
+++ my_file.java
@@ -2,7 +2,7 @@
 public class ConditionalBreakPoint  {
 
     public void breakOnConditionOnly(){
-        String[] data1 = {"mz", "my", "my", "mx", "mz", "mx", "my", "mz", "mz", "my", "my", "mx", "mz", "mx", "my", "mz"};
+        String[] data1 = {"Duplicate", "Example"};
 
         for(int i=0;i<data1.length;i++){
             String s = "Super : "+data1[i];
