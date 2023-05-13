03bff4eb75745b79bca6c4519c1c6806b0192a85
Waseemk9 <39156999+Waseemk9@users.noreply.github.com>
2023-05-12 18:22:46 -0700

    Create Code_smells_example.py

---
+++ Code_smells_example.py
@@ -0,0 +1,10 @@
+# Example for code smells
+def calculate_total_price(items):
+    total = 0
+    for item in items:
+        if item.quantity > 0:
+            if item.price > 100:
+                total += item.price * 0.9
+            else:
+                total += item.price * 0.95
+    return total
