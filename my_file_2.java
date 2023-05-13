b511fd1cd8ae964566eb0da6e7f16d7be9ba6707
Waseemk9 <39156999+Waseemk9@users.noreply.github.com>
2023-05-12 18:36:49 -0700

    Update my_file_2.java

--- my_file_2.java
+++ my_file_2.java
@@ -1,8 +1,11 @@
-# Duplication Example
+/**
+ * Basics Java code for Duplication Example
+ *
+ */
 public class ConditionalBreakPoint  {
 
     public void breakOnConditionOnly(){
-        String[] data1 = {"Duplicate", "Example"};
+        String[] data1 = {"mz", "my", "my", "mx", "mz", "mx", "my", "mz", "mz", "my", "my", "mx", "mz", "mx", "my", "mz"};
 
         for(int i=0;i<data1.length;i++){
             String s = "Super : "+data1[i];
