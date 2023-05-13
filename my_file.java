eacfe9dc831fac2db8ee2c74516ca3c06c49c5c1
Waseemk9 <39156999+Waseemk9@users.noreply.github.com>
2023-05-12 18:33:02 -0700

    Update my_file.java

--- my_file.java
+++ my_file.java
@@ -1,17 +1,17 @@
 # Duplication Example
-public class Duplication  {
+public class ConditionalBreakPoint  {
 
-    public void example(){
-        String[] data1 = {"Duplicate", "Example"};
+    public void breakOnConditionOnly(){
+        String[] data1 = {"mz", "my", "my", "mx", "mz", "mx", "my", "mz", "mz", "my", "my", "mx", "mz", "mx", "my", "mz"};
 
         for(int i=0;i<data1.length;i++){
             String s = "Super : "+data1[i];
             System.out.println(s);
-        }
+        }  
     }
     public static void main(String[] args) {
-        Duplication test = new Duplication();
-        test.example();
+        ConditionalBreakPoint test = new ConditionalBreakPoint();
+        test.breakOnConditionOnly();
     }
 
 }
